c1d8841ace41edc6267b4656bf17f48a96ac725a
Sergey Matveev <stargrave@stargrave.org>
2016-09-25 18:19:24 +0300

    Simpler 34.10 code

--- src/cypherpunks.ru/gogost/gost3410/curve.go
+++ src/cypherpunks.ru/gogost/gost3410/curve.go
@@ -22,11 +22,10 @@ import (
 )
 
 var (
-	zero *big.Int = big.NewInt(0)
+	zero    *big.Int = big.NewInt(0)
+	bigInt1 *big.Int = big.NewInt(1)
 	bigInt2 *big.Int = big.NewInt(2)
 	bigInt3 *big.Int = big.NewInt(3)
-	bigInt4 *big.Int = big.NewInt(4)
-	bigInt8 *big.Int = big.NewInt(8)
 )
 
 type Curve struct {
@@ -38,6 +37,11 @@ type Curve struct {
 	// Basic point X and Y coordinates
 	Bx *big.Int
 	By *big.Int
+
+	// Temporary variable for the add method
+	t  *big.Int
+	tx *big.Int
+	ty *big.Int
 }
 
 func NewCurve(p, q, a, b, bx, by []byte) (*Curve, error) {
@@ -48,6 +52,9 @@ func NewCurve(p, q, a, b, bx, by []byte) (*Curve, error) {
 		B:  bytes2big(b[:]),
 		Bx: bytes2big(bx[:]),
 		By: bytes2big(by[:]),
+		t:  big.NewInt(0),
+		tx: big.NewInt(0),
+		ty: big.NewInt(0),
 	}
 	r1 := big.NewInt(0)
 	r2 := big.NewInt(0)
@@ -67,120 +74,62 @@ func NewCurve(p, q, a, b, bx, by []byte) (*Curve, error) {
 	return &c, nil
 }
 
+func (c *Curve) pos(v *big.Int) {
+	if v.Cmp(zero) < 0 {
+		v.Add(v, c.P)
+	}
+}
+
+func (c *Curve) add(p1x, p1y, p2x, p2y *big.Int) {
+	if p1x.Cmp(p2x) == 0 && p1y.Cmp(p2y) == 0 {
+		// double
+		c.t.Mul(p1x, p1x)
+		c.t.Mul(c.t, bigInt3)
+		c.t.Add(c.t, c.A)
+		c.tx.Mul(bigInt2, p1y)
+		c.tx.ModInverse(c.tx, c.P)
+		c.t.Mul(c.t, c.tx)
+		c.t.Mod(c.t, c.P)
+	} else {
+		c.tx.Sub(p2x, p1x)
+		c.tx.Mod(c.tx, c.P)
+		c.pos(c.tx)
+		c.ty.Sub(p2y, p1y)
+		c.ty.Mod(c.ty, c.P)
+		c.pos(c.ty)
+		c.t.ModInverse(c.tx, c.P)
+		c.t.Mul(c.t, c.ty)
+		c.t.Mod(c.t, c.P)
+	}
+	c.tx.Mul(c.t, c.t)
+	c.tx.Sub(c.tx, p1x)
+	c.tx.Sub(c.tx, p2x)
+	c.tx.Mod(c.tx, c.P)
+	c.pos(c.tx)
+	c.ty.Sub(p1x, c.tx)
+	c.ty.Mul(c.ty, c.t)
+	c.ty.Sub(c.ty, p1y)
+	c.ty.Mod(c.ty, c.P)
+	c.pos(c.ty)
+	p1x.Set(c.tx)
+	p1y.Set(c.ty)
+}
+
 func (c *Curve) Exp(degree, xS, yS *big.Int) (*big.Int, *big.Int, error) {
-	x := big.NewInt(1)
-	y := big.NewInt(1)
-	z := big.NewInt(0)
-	lm1 := big.NewInt(0)
-	lm2 := big.NewInt(0)
-	lm3 := big.NewInt(0)
-	lm4 := big.NewInt(0)
-	lm5 := big.NewInt(0)
-	lm6 := big.NewInt(0)
-	lm7 := big.NewInt(0)
-	i := degree.BitLen() - 1
-	if i == 0 {
+	dg := big.NewInt(0).Sub(degree, bigInt1)
+	tx := big.NewInt(0).Set(xS)
+	ty := big.NewInt(0).Set(yS)
+	cx := big.NewInt(0).Set(xS)
+	cy := big.NewInt(0).Set(yS)
+	if dg.Cmp(zero) == 0 {
 		return nil, nil, errors.New("Bad degree value")
 	}
-	for ; i >= 0; i -= 1 {
-		if z.Cmp(zero) != 0 {
-			lm2.Mul(x, x)
-			lm2.Mod(lm2, c.P)
-			lm2.Mul(lm2, bigInt3)
-			lm1.Mul(z, z)
-			lm1.Mul(lm1, c.A)
-			lm1.Mod(lm1, c.P)
-			lm1.Add(lm1, lm2)
-			lm2.Mul(y, z)
-			lm2.Mod(lm2, c.P)
-			lm3.Mul(lm2, x)
-			lm3.Mul(lm3, y)
-			lm3.Mod(lm3, c.P)
-			lm5.Mul(lm3, bigInt8)
-			lm4.Mul(lm1, lm1)
-			lm4.Mod(lm4, c.P)
-			lm4.Sub(lm4, lm5)
-			lm5.Mul(lm2, bigInt2)
-			lm5.Mod(lm5, c.P)
-			lm6.Mul(lm5, lm5)
-			lm6.Mul(lm6, bigInt2)
-			lm6.Mod(lm6, c.P)
-			x.Mul(lm4, lm5)
-			x.Mod(x, c.P)
-			lm7.Mul(y, y)
-			lm7.Mod(lm7, c.P)
-			lm7.Mul(lm7, lm6)
-			y.Mul(lm3, bigInt4)
-			y.Sub(y, lm4)
-			y.Mul(y, lm1)
-			y.Sub(y, lm7)
-			y.Mod(y, c.P)
-			z.Mul(lm2, lm6)
-			z.Mod(z, c.P)
-			if x.Cmp(zero) < 0 {
-				x.Add(x, c.P)
-			}
-			if y.Cmp(zero) < 0 {
-				y.Add(y, c.P)
-			}
-			if z.Cmp(zero) < 0 {
-				z.Add(z, c.P)
-			}
+	for dg.Cmp(zero) != 0 {
+		if dg.Bit(0) == 1 {
+			c.add(tx, ty, cx, cy)
 		}
-		if degree.Bit(i) == 1 {
-			if z.Cmp(zero) == 0 {
-				x.Add(zero, xS)
-				y.Add(zero, yS)
-				z = big.NewInt(1)
-			} else {
-				lm1.Mul(yS, z)
-				lm1.Mod(lm1, c.P)
-				lm1.Sub(lm1, y)
-				lm3.Mul(xS, z)
-				lm3.Mod(lm3, c.P)
-				lm3.Sub(lm3, x)
-				lm2.Mul(lm3, lm3)
-				lm2.Mod(lm2, c.P)
-				lm4.Mul(lm2, lm3)
-				lm4.Mod(lm4, c.P)
-				lm5.Mul(bigInt2, x)
-				lm5.Mul(lm5, lm2)
-				lm5.Mod(lm5, c.P)
-				lm6.Mul(lm1, lm1)
-				lm6.Mul(lm6, z)
-				lm6.Mod(lm6, c.P)
-				lm6.Sub(lm6, lm4)
-				lm6.Sub(lm6, lm5)
-				lm5.Mul(y, lm4)
-				lm5.Mod(lm5, c.P)
-				y.Mul(lm2, x)
-				y.Sub(y, lm6)
-				y.Mul(y, lm1)
-				y.Sub(y, lm5)
-				y.Mod(y, c.P)
-				x.Mul(lm3, lm6)
-				x.Mod(x, c.P)
-				z.Mul(z, lm4)
-				z.Mod(z, c.P)
-				if x.Cmp(zero) < 0 {
-					x.Add(x, c.P)
-				}
-				if y.Cmp(zero) < 0 {
-					y.Add(y, c.P)
-				}
-				if z.Cmp(zero) < 0 {
-					z.Add(z, c.P)
-				}
-			}
-		}
+		dg.Rsh(dg, 1)
+		c.add(cx, cy, cx, cy)
 	}
-	if z.Cmp(zero) == 0 {
-		return big.NewInt(-1), big.NewInt(-1), nil
-	}
-	lm1.ModInverse(z, c.P)
-	lm2.Mul(x, lm1)
-	lm2.Mod(lm2, c.P)
-	lm3.Mul(y, lm1)
-	lm3.Mod(lm3, c.P)
-	return lm2, lm3, nil
+	return tx, ty, nil
 }
