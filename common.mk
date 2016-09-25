ee60af644b4ab2748820024de41e31b54f50c69d
Sergey Matveev <stargrave@stargrave.org>
2016-09-25 15:03:32 +0300

    Official Стрибог transcription is Streebog

--- common.mk
+++ common.mk
@@ -1,5 +1,5 @@
-gogost-stribog:
-	GOPATH=$(GOPATH) go build cypherpunks.ru/gogost/gost34112012/cmd/gogost-stribog
+gogost-streebog:
+	GOPATH=$(GOPATH) go build cypherpunks.ru/gogost/gost34112012/cmd/gogost-streebog
 
 bench:
 	GOPATH=$(GOPATH) go test -benchmem -bench . cypherpunks.ru/gogost/...
